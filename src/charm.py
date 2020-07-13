92b3548bc322b3d9c0fa5d936f4cd77f59c8985c
jamesbeedy <jamesbeedy@gmail.com>
2020-07-12 17:00:38 -0700

    moar dev

--- src/charm.py
+++ src/charm.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python3
+import logging
+
+
 from ops.charm import CharmBase
 
 from ops.main import main
@@ -8,9 +11,105 @@ from ops.model import (
     BlockedStatus,
 )
 
+from ops.framework import (
+    EventBase,
+    EventSource,
+    Object,
+    ObjectEvents,
+    StoredState,
+)
+
+
 from slurm_ops_manager import SlurmOpsManager
 
-from interface_slurm_cluster import SlurmClusterRequiresRelation
+
+logger = logging.getLogger()
+
+
+class ConfigAvailableEvent(EventBase):
+    """Slurm Available Event"""
+
+
+class SlurmdProvidesEvents(ObjectEvents):
+    """Slurm Provides Events"""
+    config_available = EventSource(ConfigAvailableEvent)
+
+
+class SlurmdProvidesRelation(Object):
+
+    on = SlurmdProvidesEvents()
+    
+    _state = StoredState()
+
+    def __init__(self, charm, relation_name):
+        super().__init__(charm, relation_name)
+        self.charm = charm
+        self._relation_name = relation_name
+
+        self._state.set_default(slurm_config=dict())
+
+        self.framework.observe(
+            self.charm.on[self._relation_name].relation_created,
+            self._on_relation_created
+        )
+
+        self.framework.observe(
+            self.charm.on[self._relation_name].relation_joined,
+            self._on_relation_joined
+        )
+
+        self.framework.observe(
+            self.charm.on[self._relation_name].relation_changed,
+            self._on_relation_changed
+        )
+
+        self.framework.observe(
+            self.charm.on[self._relation_name].relation_departed,
+            self._on_relation_departed
+        )
+
+        self.framework.observe(
+            self.charm.on[self._relation_name].relation_broken,
+            self._on_relation_broken
+        )
+
+    def _on_relation_created(self, event):
+        logger.debug("################ LOGGING RELATION CREATED ####################")
+
+        if self.charm.slurm_ops_manager.slurm_installed:
+            event.relation.data[self.model.unit]['hostname'] = \
+                self.charm.slurm_ops_manager.hostname
+            event.relation.data[self.model.unit]['inventory'] = \
+                self.charm.slurm_ops_manager.inventory
+            event.relation.data[self.model.unit]['partition'] = \
+                self.charm.config['partition']
+            event.relation.data[self.model.unit]['default'] = \
+                str(self.charm.config['default']).lower()
+        else:
+            # If we hit this hook/handler before slurm is installed, defer.
+            logger.debug("SLURM NOT INSTALLED DEFERING SETTING RELATION DATA")
+            event.defer()
+            return
+
+    def _on_relation_joined(self, event):
+        logger.debug("################ LOGGING RELATION JOINED ####################")
+
+    def _on_relation_changed(self, event):
+        logger.debug("################ LOGGING RELATION CHANGED ####################")
+
+        slurm_config = event.relation.data[event.app].get('slurm_config')
+        if slurm_config:
+            self._state.slurm_config = json.loads(slurm_config)
+            self.on.config_available.emit()
+    
+    def _on_relation_departed(self, event):
+        logger.debug("################ LOGGING RELATION DEPARTED ####################")
+
+    def _on_relation_broken(self, event):
+        logger.debug("################ LOGGING RELATION BROKEN ####################")
+
+    def get_config(self):
+        return self._state.slurm_config
 
 
 class SlurmdCharm(CharmBase):
@@ -20,28 +119,33 @@ class SlurmdCharm(CharmBase):
 
         self.slurm_ops_manager = SlurmOpsManager(self, 'slurmd')
 
-        self.slurm_cluster = SlurmClusterRequiresRelation(self, "slurm-cluster")
+        self.slurmd = SlurmdProvidesRelation(self, "slurmd")
 
         self.config = self.model.config
    
         self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.start, self._on_start)
 
+        self.framework.observe(self.slurmd.on.config_available, self._on_config_available)
+
 
     def _on_install(self, event):
         self.slurm_ops_manager.prepare_system_for_slurm()
+        self.unit.status = ActiveStatus("Slurm Installed")
 
     def _on_start(self, event):
-        if self.slurm_cluster.slurm_config_acquired and self.slurm_ops_manager.slurm_installed:
-            self.unit.status = ActiveStatus("Slurmd Available")
-        else:
-            if not self.slurm_cluster.slurm_config_acquired:
-                self.unit.status = BlockedStatus("Need relation to slurm controller.")
-            elif not self.slurm_ops_manager.slurm_installed:
-                self.unit.status = WaitingStatus("Waiting on slurm install to complete...")
-            event.defer()
-            return 
+        pass
 
+    def _on_config_available(self, event):
+        if self.slurm_ops_manager.slurm_installed:
+            slurm_config = self.slurmd.get_config()
+            #self.charm.slurm_ops_manager.render_config_and_restart(slurm_config)
+            logger.debug(slurm_config)
+            self.unit.status = ActiveStatus("Slurm config available")
+        else:
+            self.unit.status = BlockedStatus("Blocked slurm not installed yet")
+            event.defer()
+            return
 
 if __name__ == "__main__":
     main(SlurmdCharm)
