7e3ddd58298cb057156193475f752a033327e42f
jesse <jesse1517@gmail.com>
2020-07-14 22:22:07 +0000

    linted

--- src/charm.py
+++ src/charm.py
@@ -1,47 +1,46 @@
 #! /usr/bin/env python3
-import json
-import logging
-
-
+"""libraries needed for charm."""
+from interface_slurmd import SlurmdProvides
 from ops.charm import CharmBase
-
 from ops.main import main
-
 from ops.model import (
     ActiveStatus,
     BlockedStatus,
 )
-
 from slurm_ops_manager import SlurmOpsManager
-
-from interface_slurmd import SlurmdProvides
+from stdlib import (
+    json,
+    logging,
+)
 
 logger = logging.getLogger()
 
 
 class SlurmdCharm(CharmBase):
+    """representing the comtpute node portion of Slurm Workload Manager."""
 
     def __init__(self, *args):
+        """Initialize charm and configure states and events to observe.
+
+        Component: SlurmOpsManager class that installs the slurm binary.
+        Interface: SlurmdProvides which is responsible for communicating
+        with slurmctld.
+        """
         super().__init__(*args)
-
         self.slurm_ops_manager = SlurmOpsManager(self, 'slurmd')
-
         self.slurmd = SlurmdProvides(self, "slurmd")
 
         self.config = self.model.config
-   
         self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.start, self._on_config_available)
 
         self.framework.observe(self.slurmd.on.config_available, self._on_config_available)
-
 
     def _on_install(self, event):
         self.slurm_ops_manager.prepare_system_for_slurm()
         self.unit.status = ActiveStatus("Slurm Installed")
 
     def _on_config_available(self, event):
-
         if (self.slurm_ops_manager.slurm_installed and self.slurmd.config_available):
 
             try:
